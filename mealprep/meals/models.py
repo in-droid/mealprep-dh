f55b3536805c810e5579cb235bb4fe4ad5c45fdc
R4d0slav <radoslav.atanasoski@gmail.com>
2022-05-14 14:44:25 +0200

    databases created

--- mealprep/meals/models.py
+++ mealprep/meals/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class Foods(models.Model):
+    fid = models.PrimaryKey()
+    name = models.CharField(max_length=256)
+    volume = models.IntegerField()
+    calories = models.IntegerField()
+    protein = models.IntegerField()
+
+class User_Fridge(models.Model):
+    uid = models.PrimaryKey()
+    fid = models.ForeignKey(Foods, on_delete=models.CASCADE)
+    volume = models.IntegerField()
+
+class Recipes(models.Model):
+    fid = models.PrimaryKey()
+    name = models.CharField(1024)
+    directions = models.CharField(1024)
+    ingridients = models.CharField(1024)
+    type = models.CharField(16)
+    time = models.IntegerField()
+    image = models.CharField(256)
+
+
