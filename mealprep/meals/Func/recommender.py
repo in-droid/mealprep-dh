223d58fea92007189978757077a0543bb265219c
R4d0slav <radoslav.atanasoski@gmail.com>
2022-05-14 16:51:11 +0200

    added database query class

---
+++ mealprep/meals/Func/recommender.py
@@ -0,0 +1,15 @@
+from meals.models import *
+
+class Recommender:
+    def __init__(self):
+        pass
+
+    def _getUserFridge(self):
+        return 
+
+
+
+
+
+
+# Recommender()
--- mealprep/meals/models.py
+++ mealprep/meals/models.py
@@ -1,26 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Foods(models.Model):
-    fid = models.PrimaryKey()
+    # fid = models.AutoField(primary_key=True)
     name = models.CharField(max_length=256)
     volume = models.IntegerField()
-    calories = models.IntegerField()
-    protein = models.IntegerField()
+    calories = models.IntegerField(null=True)
+    protein = models.IntegerField(null=True)
+
+    def __str__(self):
+        return self.name
 
 class User_Fridge(models.Model):
-    uid = models.PrimaryKey()
-    fid = models.ForeignKey(Foods, on_delete=models.CASCADE)
+    uid = models.ForeignKey(User, on_delete=models.CASCADE)
+    fid = models.ForeignKey(Foods, on_delete=models.CASCADE, unique=True)
     volume = models.IntegerField()
 
+    def __str__(self):
+        return str(self.uid) + ', ' + str(self.fid)
+    
+    class Meta:
+        unique_together = ('uid', 'fid')
+
 class Recipes(models.Model):
-    fid = models.PrimaryKey()
-    name = models.CharField(1024)
-    directions = models.CharField(1024)
-    ingridients = models.CharField(1024)
-    type = models.CharField(16)
-    time = models.IntegerField()
-    image = models.CharField(256)
+    # fid = models.AutoField(primary_key=True)()
+    name = models.CharField(max_length=1024)
+    directions = models.CharField(max_length=1024)
+    ingridients = models.CharField(max_length=1024)
+    type = models.CharField(max_length=16)
+    time = models.IntegerField(null=True)
+    image = models.CharField(max_length=256, null=True)
+
+    def __str__(self):
+        return models.name
 
 
