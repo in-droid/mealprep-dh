77555eb0048215bcf0db9e1ffa2f1d4e257e06f5
Ivan Nikolov <61251589+in-droid@users.noreply.github.com>
2022-05-15 11:48:33 +0200

    Delete mealprep directory

--- mealprep/meals/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from .models import *
-
-admin.site.register(Foods)
-admin.site.register(User_Fridge)
-admin.site.register(Recipes)
